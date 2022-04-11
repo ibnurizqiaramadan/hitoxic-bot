465e07069c4ab7c7e6ac31cedf2513892507679d
ibnurizqiaramadan <dadanibnu61@gmail.com>
2022-04-12 00:10:14 +0700

    fix ping and avatar

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -1,49 +1,10 @@
 const Command = require('../structures/Command')
-const { MessageEmbed } = require("discord.js")
 
 module.exports = new Command({
-	name: "avatar", 
-	description: "get player avatar", 
+	name: "ping", 
+    aliases: "pi",
+	description: "Ping", 
 	async run(message, args, client) {
-		let colorEmbed, authorUrl;
-        if (message.member.displayColor) {
-            colorEmbed = message.member.displayColor
-        } else {
-            colorEmbed = 3447003
-        }
-		// console.log(message.author);
-        authorUrl = `https://cdn.discordapp.com/avatars/${message.author.id}/${message.author.avatar}.png?size=1024`
-        function kirim(avatar, title) {
-            message.channel.send({
-				embeds: [
-					new MessageEmbed()
-						.setTitle(title)
-						.setURL(avatar)
-						.setFooter({
-							iconURL: authorUrl,
-							text: `Requested by ${message.author.tag}`
-						})
-						.setImage(avatar)
-				]
-			})
-        }
-        if (args.length > 1) {
-            let userId;
-            args.forEach(item => {
-                if (item == "server") {
-                    avatarURL = message.guild.iconURL();
-                    kirim(avatarURL, "Icon Server Url");
-                } else {
-                    if (!/[<@!&>]/.test(item)) return
-                    userId = item.replace(/[<@!&>]/g, '');
-                    message.guild.members.fetch(userId).then(userData => {
-                        avatarURL = `https://cdn.discordapp.com/avatars/${userData.user.id}/${userData.user.avatar}.png?size=1024`;
-                        kirim(avatarURL, `Avatar Url for ${userData.user.username}`);
-                    })
-                }
-            });
-        } else {
-            kirim(authorUrl, `Avatar Url for ${message.author.tag}`)
-        }
+		message.reply(`Ping: ${client.ws.ping} ms`)
 	}
 })
