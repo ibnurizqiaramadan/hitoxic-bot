0ee3f1f3518b0c5dd762096fe55e6c13d2c67653
ibnurizqiaramadan <dadanibnu61@gmail.com>
2022-04-12 15:59:09 +0700

    remove player events

--- src/app.js
+++ src/app.js
@@ -4,14 +4,4 @@ const Client = require('./structures/Clients')
 const client = new Client()
 console.log("Starting Bot . . .")
 
-client.start(process.env.BOT_TOKEN2)
-
-client.player.on("trackStart", queue => {
-    const track = queue.current
-    if (!queue.repeatMode == 3) client.channels.cache.get(track.channel).send(`Sedang memutar **${ track.title }**`)
-})
-
-client.player.on("trackAdd", queue => {
-    const track = queue.tracks[0]
-    client.channels.cache.get(track.channel).send(`**${ track.title }** berhasil ditambahkan`)
-})
+client.start(process.env.BOT_TOKEN1)
