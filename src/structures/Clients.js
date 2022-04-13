a88e0bbb2f72cfe3ea5d0c7deedd9f05b3330562
ibnurizqiaramadan <dadanibnu61@gmail.com>
2022-04-13 15:55:30 +0700

    add express and socket.io

--- src/structures/Clients.js
+++ src/structures/Clients.js
@@ -1,9 +1,14 @@
 const Discord = require('discord.js')
+const dotenv = require('dotenv')
 const intents = new Discord.Intents(32767)
 const Command = require('./Command')
 const Event = require('./Events')
 const fs = require('fs')
 const { Player } = require("discord-player")
+const express = require('express')
+const { io } = require("socket.io-client")
+const path = require('path')
+dotenv.config();
 
 class Client extends Discord.Client {
     constructor() {
@@ -20,9 +25,17 @@ class Client extends Discord.Client {
                 highWaterMark: 1 << 25
             },
         })
+        this.webControl = express()
+        this.webControlPort = process.env.WEBCONTROL_PORT
+        this.webControlUrl = process.env.WEBCONTROL_URL
+        this.socket = io(process.env.WEBCONTROL_SOCKET)
+        this.webControl.use(express.static('./src/public'))
+        this.webControl.set('views', path.join(`./src`, 'views'))
+        this.webControl.set('view engine', 'pug')
+        this.musicPaused = false
     }
 
-    start(token) {
+    start() {
         fs.readdirSync('./src/commands').filter(file => file.endsWith('.js')).forEach(file => {
             /**
              * @type {Command}
@@ -39,7 +52,14 @@ class Client extends Discord.Client {
             console.log(`Events ${event.event} loaded`);
             this.on(event.event, event.run.bind(null, this))
         })
-        this.login(token)
+        this.login(process.env.BOT_TOKEN2)
+        this.webControl.listen(this.webControlPort, () => {
+            console.log(`Web Control run on port ${this.webControlPort}`)
+        })
+        this.socket.on("connect", () => {
+            console.log(`Socket.io connected to ${process.env.WEBCONTROL_SOCKET}`);
+        })
+        
     }
 }
 
