ea6ec441d6ecf95c8b6c8df15a4a6c111850112a
ibnurizqiaramadan <dadanibnu61@gmail.com>
2022-04-13 15:58:51 +0700

    translate

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -4,6 +4,6 @@ module.exports = new Event('messageCreate', (client, message) => {
     if (!message.content.startsWith(client.prefix)) return
 	const args = message.content.substring(client.prefix.length).split(/ +/)
 	const command = client.commands.find(cmd => cmd.name == args[0] || cmd.aliases == args[0])
-	if (!command) return message.reply(`Command tidak ditemukan :(`)
+	if (!command) return message.reply(`Command not found :(`)
 	command.run(message, args, client)
 })	
