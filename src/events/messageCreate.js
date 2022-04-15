4ccc0728fd29c3f0b0a6ffbb9e621d9bfe09c996
ibnurizqiaramadan <dadanibnu61@gmail.com>
2022-04-15 08:33:06 +0700

    prevent from bot message

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -2,6 +2,7 @@ const Event = require('../structures/Events')
 
 module.exports = new Event('messageCreate', (client, message) => {
     if (!message.content.startsWith(client.prefix)) return
+	if (message.author.bot) return
 	const args = message.content.substring(client.prefix.length).split(/ +/)
 	const command = client.commands.find(cmd => cmd.name == args[0] || cmd.aliases == args[0])
 	if (!command) return message.reply(`Command not found :(`)
