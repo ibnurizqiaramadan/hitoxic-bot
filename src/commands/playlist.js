b913b2f880652b23f21bb7c1bd6d24e99a96d7d0
ibnurizqiaramadan <dadanibnu61@gmail.com>
2022-04-15 08:33:56 +0700

    no longer used

--- src/commands/playlist.js
+++ src/commands/playlist.js
@@ -1,26 +1,11 @@
 const Command = require('../structures/Command')
-const { QueryType } = require("discord-player")
 
 module.exports = new Command({
 	name: "playlist", 
     aliases: "pl",
-	description: "Play youtube playist or spotify playlist", 
+	description: "Play youtube playist or spotify playlist (no longer used)", 
     usage: "playlist <url>",
 	async run(message, args, client) {
-		if (!message.member.voice.channel) return message.reply(`You must be on the voice channel`)
-        const queue = await client.player.createQueue(message.guild)
-		if (!queue.connection) await queue.connect(message.member.voice.channel)
-        const query = args.slice(1).join(' ')
-        const result = await client.player.search(query, {
-            requestedBy: message.author,
-            searchEngine: QueryType.AUTO
-        })
-
-        if (result.tracks.length === 0) return message.reply("No results")
-        const playlist = result.playlist
-        await queue.addTracks(result.tracks)
-        message.reply(`**${ playlist.title }** with **${result.tracks.length}** songs added !`)
-        if (!queue.playing) await queue.play()
-        client.getQueueStatus(message.guild.id)
+		message.reply(`use ${client.prefix}play instead of ${client.prefix}playlist`)
 	}
 })
