6ba904e4cc7b8641265da30eec61cf98c219f108
ibnurizqiaramadan <dadanibnu61@gmail.com>
2022-04-16 16:13:12 +0700

    prevent crash form invalidtrack

--- src/commands/play.js
+++ src/commands/play.js
@@ -7,32 +7,38 @@ module.exports = new Command({
 	description: "Play music on youtube", 
     usage: "play <url|title>",
 	async run(message, args, client) {
-		if (!message.member.voice.channel) return message.reply(`You must be on the voice channel`)
+		try {
+            if (!message.member.voice.channel) return message.reply(`You must be on the voice channel`)
 
-        const queue = await client.player.createQueue(message.guild)
-		if (!queue.connection) await queue.connect(message.member.voice.channel)
-        const query = args.slice(1).join(' ')
-        console.log(query);
-        const queryType = QueryResolver.resolve(query)
-        const result = await client.player.search(query, {
-            requestedBy: message.author,
-            searchEngine: queryType
-        })
-        // console.log(result);
-        if (result.playlist) {
-            const playlist = result.playlist
-            const tracks = result.tracks.slice(0, 200)
-            await queue.addTracks(tracks)
-            message.reply(`**${ playlist.title }** with **${tracks.length}** songs added !`)
-            console.log('playlist loaded', message.guild.id, message.guild.name);
+            const queue = await client.player.createQueue(message.guild)
+            if (!queue.connection) await queue.connect(message.member.voice.channel)
+            const query = args.slice(1).join(' ')
+            console.log(query);
+            const queryType = QueryResolver.resolve(query)
+            const result = await client.player.search(query, {
+                requestedBy: message.author,
+                searchEngine: queryType
+            })
+            // console.log(result);
+            if (result.playlist) {
+                const playlist = result.playlist
+                console.log(result.tracks);
+                const tracks = result.tracks.slice(0, 200)
+                await queue.addTracks(tracks)
+                message.reply(`**${ playlist.title }** with **${tracks.length}** songs added !`)
+                console.log('playlist loaded', message.guild.id, message.guild.name);
+            }
+            else {
+                const track = result.tracks[0]
+                await queue.addTrack(track)
+                message.reply(`**${ track.title }** added !`)
+                console.log('track loaded', message.guild.id, message.guild.name);
+            } 
+            if (!queue.playing) await queue.play()
+            client.getQueueStatus(message.guild.id)
+        } catch (error) {
+            console.error(error);
+            message.reply(`Error : **${error?.statusCode}**`)
         }
-        else {
-            const track = result.tracks[0]
-            await queue.addTrack(track)
-            message.reply(`**${ track.title }** added !`)
-            console.log('track loaded', message.guild.id, message.guild.name);
-        } 
-        if (!queue.playing) await queue.play()
-        client.getQueueStatus(message.guild.id)
 	}
 })
