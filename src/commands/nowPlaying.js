9182b772b2b76b40470d21fa4394b33db3d1da58
ibnurizqiaramadan <dadanibnu61@gmail.com>
2022-04-25 17:06:52 +0700

    add try catch on nowplaying command

--- src/commands/nowPlaying.js
+++ src/commands/nowPlaying.js
@@ -6,24 +6,28 @@ module.exports = new Command({
 	description: "Display current playing song", 
 	usage: "nowplaying",
 	async run(message, args, client) {
-		if (!message.member.voice.channel) return message.reply(`You must be on the voice channel`)
-        const queue = await client.player.getQueue(message.guild)
-		if (!queue) return await message.reply("Queue is empty")
+		try {
+			if (!message.member.voice.channel) return message.reply(`You must be on the voice channel`)
+			const queue = await client.player.getQueue(message.guild)
+			if (!queue) return await message.reply("Queue is empty")
 
-		const bar = queue.createProgressBar({
-			queue: false,
-			length: 19,
-		})
+			const bar = queue.createProgressBar({
+				queue: false,
+				length: 19,
+			})
 
-		const time = queue.getPlayerTimestamp()
+			const time = queue.getPlayerTimestamp()
 
-        const song = queue.current
+			const song = queue.current
 
-		await message.reply({
-			embeds: [client.MessageEmbed
-				.setThumbnail(song.thumbnail)
-				.setDescription(`Currently playing [${song.title}](${song.url})\n\n${time.current} ${bar} ${time.end}`)
-        	],
-		})
+			await message.reply({
+				embeds: [client.MessageEmbed
+					.setThumbnail(song.thumbnail)
+					.setDescription(`Currently playing [${song.title}](${song.url})\n\n${time.current} ${bar} ${time.end}`)
+				],
+			})
+		} catch (error) {
+			message.reply(`❌ Error while processing the action`)	
+		}
 	}
 })
