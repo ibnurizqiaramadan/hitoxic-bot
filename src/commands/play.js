f7c2a1e7ef7341f16e50b8b04fe58d15ab20d61d
Ibnurizqiaramadan <dadanibnu61@gmail.com>
2022-07-28 19:20:32 +0700

    change description and move message reply

--- src/commands/play.js
+++ src/commands/play.js
@@ -1,53 +1,66 @@
-const Command = require('../structures/Command')
-const { QueryResolver } = require("discord-player")
+const Command = require("../structures/Command");
+const { QueryResolver } = require("discord-player");
 
 module.exports = new Command({
-	name: "play", 
+    name: "play",
     aliases: "p",
-	description: "Play music on youtube", 
+    description: "Play music on Youtube, Spotify or Soundcloud",
     usage: "play <url|title>",
-	async run(message, args, client) {
-        let msg
-		try {
-            if (!message.member.voice.channel) return message.reply(`You must be on the voice channel`)
+    async run(message, args, client) {
+        let msg = await message.reply(`🔃 Processing query...`);
+        try {
+            if (!message.member.voice.channel)
+                return message.reply(`You must be on the voice channel`);
             const queue = await client.player.createQueue(message.guild, {
                 leaveOnEmpty: false,
                 leaveOnEnd: false,
                 leaveOnStop: false,
                 ytdlOptions: {
                     quality: "highestaudio",
-                    highWaterMark: 1 << 25
+                    highWaterMark: 1 << 25,
                 },
-            })
-            if (!queue.connection) await queue.connect(message.member.voice.channel)
-            client.serverQueueSettings[message.guild.id] = {channel: message.channel.id}
-            const query = args.slice(1).join(' ')
+            });
+            if (!queue.connection)
+                await queue.connect(message.member.voice.channel);
+            client.serverQueueSettings[message.guild.id] = {
+                channel: message.channel.id,
+                voice: message.member.voice.channel,
+            };
+            const query = args.slice(1).join(" ");
             console.log(query);
-            msg = await message.reply(`🔃 Processing query...`)
-            const queryType = QueryResolver.resolve(query)
+            const queryType = QueryResolver.resolve(query);
             const result = await client.player.search(query, {
                 requestedBy: message.author,
-                searchEngine: queryType
-            })
+                searchEngine: queryType,
+            });
             // console.log(result);
             if (result.playlist) {
-                const playlist = result.playlist
-                const tracks = result.tracks.slice(0, 200)
-                await queue.addTracks(tracks)
-                msg.edit(`✅ **${ playlist.title }** with **${tracks.length}** songs added !`)
-                console.log('playlist loaded', message.guild.id, message.guild.name);
+                const playlist = result.playlist;
+                const tracks = result.tracks.slice(0, 200);
+                await queue.addTracks(tracks);
+                msg.edit(
+                    `✅ **${playlist.title}** with **${tracks.length}** songs added !`
+                );
+                console.log(
+                    "playlist loaded",
+                    message.guild.id,
+                    message.guild.name
+                );
+            } else {
+                const track = result.tracks[0];
+                await queue.addTrack(track);
+                msg.edit(`✅ **${track.title}** added !`);
+                console.log(
+                    "track loaded",
+                    message.guild.id,
+                    message.guild.name
+                );
             }
-            else {
-                const track = result.tracks[0]
-                await queue.addTrack(track)
-                msg.edit(`✅ **${ track.title }** added !`)
-                console.log('track loaded', message.guild.id, message.guild.name);
-            } 
-            if (!queue.playing) await queue.play()
-            client.getQueueStatus(message.guild.id)
+            if (!queue.playing) await queue.play();
+            client.getQueueStatus(message.guild.id);
         } catch (error) {
             console.error(error);
-            msg.edit(`❌ Error : **${error?.statusCode ?? "Unknown"}**`)
+            msg.edit(`❌ Error : **${error?.statusCode ?? "Unknown"}**`);
         }
-	}
-})
+    },
+});
