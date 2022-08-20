c651e4739e170149127a64250fd26cb93f300794
Ibnurizqiaramadan <dadanibnu61@gmail.com>
2022-08-21 00:32:22 +0700

    reformat and add clientTrackDelete

--- server/server.js
+++ server/server.js
@@ -1,89 +1,93 @@
-const express = require('express')
-const app = express()
-const server = require('http').Server(app)
-const io = require('socket.io')(server, {
-  cors: {
-    origin: '*',
-  }
-})
-const dotenv = require('dotenv');
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server, {
+    cors: {
+        origin: "*",
+    },
+});
+const dotenv = require("dotenv");
 dotenv.config();
-app.get('/', (req, res) => {
-    res.send(":)")
-})
+app.get("/", (req, res) => {
+    res.send(":)");
+});
 
-io.on('connection', socket => {
+io.on("connection", (socket) => {
     console.log("user connected", socket.id);
-    
-    socket.on('join', guildId => {
-        socket.join(guildId)
+
+    socket.on("join", (guildId) => {
+        socket.join(guildId);
         console.log(`user ${socket.id} join room ${guildId}`);
-        io.emit("getStatus", guildId)
-    })
+        io.emit("getStatus", guildId);
+    });
 
     // server to client
-    socket.on("startTrack", data => {
-        io.to(data.guild).emit("startNewTrack", data)
-    })
+    socket.on("startTrack", (data) => {
+        io.to(data.guild).emit("startNewTrack", data);
+    });
 
-    socket.on("serverSendTime", data => {
-        io.to(data.guild).emit("receiveTime", data.time)
-    })
+    socket.on("serverSendTime", (data) => {
+        io.to(data.guild).emit("receiveTime", data.time);
+    });
 
-    socket.on("serverSendStatus", data => {
-        io.to(data.guild).emit("receiveStatus", data)
-    })
+    socket.on("serverSendStatus", (data) => {
+        io.to(data.guild).emit("receiveStatus", data);
+    });
 
-    socket.on("serverSendPlayRepeatStatus", data => {
-        io.to(data.guild).emit("receivePlayRepeatStatus", data.status)
-    })
+    socket.on("serverSendPlayRepeatStatus", (data) => {
+        io.to(data.guild).emit("receivePlayRepeatStatus", data.status);
+    });
 
-    socket.on("serverSendQueue", data => {
-        io.to(data.guild).emit("receiveQueue", data.queue)
-    })
+    socket.on("serverSendQueue", (data) => {
+        io.to(data.guild).emit("receiveQueue", data.queue);
+    });
 
-    socket.on("serverSendQueueMessage", data => {
-        io.to(data.guild).emit("receiveQueueMessage", data.message)
-    })
+    socket.on("serverSendQueueMessage", (data) => {
+        io.to(data.guild).emit("receiveQueueMessage", data.message);
+    });
 
-    
-    // client to server 
-    socket.on('clientGetTime', guildId => {
-        io.emit("serverGetTime", guildId)
-    })
+    // client to server
+    socket.on("clientGetTime", (guildId) => {
+        io.emit("serverGetTime", guildId);
+    });
 
-    socket.on('clientShuffleQueue', guildId => {
-        io.emit("serverShuffleQueue", guildId)
-    })
-    
-    socket.on('clientNextQueue', guildId => {
-        io.emit("serverNextQueue", guildId)
-    })
+    socket.on("clientShuffleQueue", (guildId) => {
+        io.emit("serverShuffleQueue", guildId);
+    });
 
-    socket.on('clientPreviousQueue', guildId => {
-        io.emit("serverPreviousQueue", guildId)
-    })
+    socket.on("clientNextQueue", (guildId) => {
+        io.emit("serverNextQueue", guildId);
+    });
 
-    socket.on('clientPlayPause', guildId => {
-        io.emit("serverPlayPause", guildId)
-    })
+    socket.on("clientPreviousQueue", (guildId) => {
+        io.emit("serverPreviousQueue", guildId);
+    });
 
-    socket.on('clientRepeat', guildId => {
-        io.emit("serverRepeat", guildId)
-    })
+    socket.on("clientPlayPause", (guildId) => {
+        io.emit("serverPlayPause", guildId);
+    });
 
-    socket.on("clientSeek", data => {
-        io.emit("serverSeek", data)
-    })
+    socket.on("clientRepeat", (guildId) => {
+        io.emit("serverRepeat", guildId);
+    });
 
-    socket.on("clientTrackSelected", data => {
-        io.emit("serverTrackSelected", data)
-    })
+    socket.on("clientSeek", (data) => {
+        io.emit("serverSeek", data);
+    });
 
-    socket.on("clientInputQuery", data => {
+    socket.on("clientTrackSelected", (data) => {
+        io.emit("serverTrackSelected", data);
+    });
+
+    socket.on("clientTrackDelete", (data) => {
+        console.log(data);
+        io.emit("serverTrackDelete", data);
+    });
+
+    socket.on("clientInputQuery", (data) => {
         // console.log(data);
-        io.emit("serverInputQuery", data)
-    })
-})
+        io.emit("serverInputQuery", data);
+    });
+});
 
-server.listen(process.env.SOCKET_PORT)
+server.listen(process.env.SOCKET_PORT);
