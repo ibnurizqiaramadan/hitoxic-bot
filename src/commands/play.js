8e565691dd66c6caf04aa5ffbd46098193f49efc
Ibnurizqiaramadan <dadanibnu61@gmail.com>
2022-11-21 20:28:48 +0700

    play changes + add error prevent on query is empty + add multiple input query

--- src/commands/play.js
+++ src/commands/play.js
@@ -27,37 +27,48 @@ module.exports = new Command({
                 voice: message.member.voice.channel,
             };
             const query = args.slice(1).join(" ");
+            if (query.trim() == "") return msg.edit(
+                `❌ Enter title or link !`
+            );
             console.log(query);
-            const queryType = QueryResolver.resolve(query);
-            const result = await client.player.search(query, {
-                requestedBy: message.author,
-                searchEngine: queryType,
-            });
-            // console.log(result);
-            if (result.playlist) {
-                const playlist = result.playlist;
-                const tracks = result.tracks.slice(0, 200);
-                await queue.addTracks(tracks);
-                msg.edit(
-                    `✅ **${playlist.title}** with **${tracks.length}** songs added !`
-                );
-                console.log(
-                    "playlist loaded",
-                    message.guild.id,
-                    message.guild.name
-                );
-            } else {
-                const track = result.tracks[0];
-                await queue.addTrack(track);
-                msg.edit(`✅ **${track.title}** added !`);
-                console.log(
-                    "track loaded",
-                    message.guild.id,
-                    message.guild.name
-                );
-            }
-            if (!queue.playing) await queue.play();
-            client.getQueueStatus(message.guild.id);
+            const arrayQuery = query.split("|")
+            let counter = 0
+            arrayQuery.forEach(async q => {
+                const queryType = QueryResolver.resolve(q);
+                const result = await client.player.search(q, {
+                    requestedBy: message.author,
+                    searchEngine: queryType,
+                });
+                // console.log(result);
+                if (result.playlist) {
+                    const playlist = result.playlist;
+                    const tracks = result.tracks.slice(0, 200);
+                    await queue.addTracks(tracks);
+                    msg.edit(
+                        `✅ **${playlist.title}** with **${tracks.length}** songs added !`
+                    );
+                    console.log(
+                        "playlist loaded",
+                        message.guild.id,
+                        message.guild.name
+                    );
+                    counter++;
+                } else {
+                    const track = result.tracks[0];
+                    await queue.addTrack(track);
+                    msg.edit(`✅ **${track.title}** added !`);
+                    console.log(
+                        "track loaded",
+                        message.guild.id,
+                        message.guild.name
+                    );
+                    counter++;
+                }
+                if (counter == arrayQuery.length) {
+                    if (!queue.playing) await queue.play();
+                    client.getQueueStatus(message.guild.id);
+                }
+            })
         } catch (error) {
             console.error(error);
             msg.edit(`❌ Error : **${error?.statusCode ?? "Unknown"}**`);
